439e808dbf118898d32e87ecdcc8f54e0d79fc9d
@thien <thien@users.noreply.github.com>
2019-04-19 01:39:06 +0100

    add shell batch to train models while im away

---
+++ code/batch_train_seq2seq.sh
@@ -0,0 +1,3 @@
+python3 train.py --ep 10 -ls 1 -r 16 --dataset subtitles --model seq2seq
+python3 train.py --ep 10 -ls 1 -r 16 --dataset amazon --model seq2seq
+python3 train.py --ep 10 -ls 1 -r 16 --dataset penn --model seq2seq
---
+++ code/batch_train_vad.sh
@@ -0,0 +1,3 @@
+python3 train.py --ep 10 -r 16 --dataset subtitles --model vad
+python3 train.py --ep 10 -r 16 --dataset amazon --model vad
+python3 train.py --ep 10 -r 16 --dataset penn --model vad
