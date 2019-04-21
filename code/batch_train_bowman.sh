06db1890a19358a4b4bde404f2d871ea174d7035
@thien <thien@users.noreply.github.com>
2019-04-21 01:35:20 +0100

    create separate shell for bowman

---
+++ code/batch_train_bowman.sh
@@ -0,0 +1,3 @@
+python3 train.py --ep 10 -r 16 --dataset subtitles --model bowman
+python3 train.py --ep 10 -r 16 --dataset amazon --model bowman
+python3 train.py --ep 10 -r 16 --dataset penn --model bowman
--- code/batch_train_seq2seq.sh
+++ code/batch_train_seq2seq.sh
@@ -1,3 +1,3 @@
-python3 train.py --ep 10 -ls 1 -r 16 --dataset subtitles --model seq2seq
-python3 train.py --ep 10 -ls 1 -r 16 --dataset amazon --model seq2seq
-python3 train.py --ep 10 -ls 1 -r 16 --dataset penn --model seq2seq
+python3 train.py --ep 10 -ul f -r 16 --dataset subtitles --model seq2seq
+python3 train.py --ep 10 -ul f -r 16 --dataset amazon --model seq2seq
+python3 train.py --ep 10 -ul f -r 16 --dataset penn --model seq2seq
