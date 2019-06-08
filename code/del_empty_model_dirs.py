503bae34f3368825d212ae3713288601c8402ee9
@thien <thien@users.noreply.github.com>
2019-06-08 20:56:44 +0100

    remove cruft code

--- code/del_empty_model_dirs.py
+++
@@ -1,8 +0,0 @@
-# deletes models with no weights saved to them.
-import shutil
-import os
-for x in os.listdir('models'):
-    subpath = os.path.join('models', x)
-    if "encoder.pth" not in os.listdir(subpath):
-        print("bad", subpath, os.listdir(subpath))
-        shutil.rmtree(subpath)
