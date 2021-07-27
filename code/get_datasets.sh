f5b607116915e54d4d9319b293d97d881cce026d
thien <thien@users.noreply.github.com>
2021-07-27 21:55:15 +0100

    initial commit for dataset_fetch

---
+++ code/get_datasets.sh
@@ -0,0 +1,12 @@
+# create dataset dir
+mkdir -p datasets 
+mkdir -p datasets/glove
+
+# get glove
+wget https://nlp.stanford.edu/data/glove.6B.zip datasets/glove
+unzip datasets/glove/glove.6B.zip
+
+# get ptb dataset
+
+# get 
+
